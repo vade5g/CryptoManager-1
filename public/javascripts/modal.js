6b2f44fe2b00ac139ba052e0d3cca1d46187625b
opamus <ollilehmusvuori@gmail.com>
2018-05-06 14:33:12 +0300

    modal for new asset working

---
+++ public/javascripts/modal.js
@@ -0,0 +1,16 @@
+let modal = document.getElementById('myModal');
+let btn = document.getElementById('myBtn');
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = () => {
+    modal.style.display = "block";
+};
+span.onclick = () => {
+    modal.style.display = "none";
+};
+
+window.onclick = (event) => {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
